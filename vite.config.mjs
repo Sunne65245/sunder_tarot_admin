9000cea8b87dccb456e6ca846d86462b065b4300
sunne <sun65245@gmail.com>
2025-02-26 02:37:56 +0800

    修改 vite 設定

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import jquery from 'jquery';
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/sunder_tarot_admin/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -18,9 +19,9 @@ export default defineConfig({
     }
   },
   define: {
-    global: {},
-    $: jquery,
-    jQuery: jquery,
-    'window.jQuery': jquery
+    global: {}, 
+    $: 'window.jQuery', 
+    jQuery: 'window.jQuery',
+    'window.jQuery': 'window.jQuery'
   }
 });
